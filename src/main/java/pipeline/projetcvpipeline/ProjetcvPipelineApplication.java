1c2076936202a870ad62f032b2102adb2b035489
Gastephen1 <sadiomn01@gmail.com>
2023-03-04 02:07:58 +0100

    first commit

---
+++ src/main/java/pipeline/projetcvpipeline/ProjetcvPipelineApplication.java
@@ -0,0 +1,13 @@
+package pipeline.projetcvpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetcvPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetcvPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pipeline/projetcvpipeline/ProjetcvPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package pipeline.projetcvpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetcvPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
